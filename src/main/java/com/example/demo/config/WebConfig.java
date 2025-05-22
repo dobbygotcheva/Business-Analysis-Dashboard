bb84ac54a9777340152409e0fac503f9943cdc1e
dobbygotcheva <146433766+dobbygotcheva@users.noreply.github.com>
2025-05-22 20:19:01 +0300

    Add files via upload

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -15,12 +17,21 @@ public class WebConfig implements WebMvcConfigurer {
     private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
     @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        logger.info("Configuring view resolvers");
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".xhtml");
+        registry.viewResolver(resolver);
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         logger.info("Configuring view controllers");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        registry.addViewController("/").setViewName("forward:/index.xhtml");
-        registry.addViewController("/index").setViewName("forward:/index.xhtml");
-        registry.addViewController("/index.xhtml").setViewName("forward:/index.xhtml");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/index.xhtml").setViewName("index");
     }
 
     @Override
